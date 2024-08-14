2c56fc59276d73e5340b002400c4abffbdb93c6a
Nate Miller <nmiller2379@gmail.com>
2024-08-14 08:27:23 -0400

    Added a couple explanatory comments

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -6,8 +6,11 @@ export default function TodoList() {
   const [todos, setTodos] = useState([]);
   useEffect(() => {
     axios
+      // Fetching data from the API. It will return a promise.
       .get("https://dummyjson.com/todos?limit=5")
+      //   When that promise is resolved, the then method is called with the response data, allowing you to handle the successful retrieval of data.
       .then((response) => {
+        //   Set the state of the todos array with the data from the API. This is considered a side effect. It is common to use the useEffect hook to handle side effects in functional components. It is also common to set the state of the component with the data from the API.
         setTodos(response.data.todos);
       })
       .catch((error) => {
