5789cde7757d2d28d92e3b3fcf8a7cb3d935c2ad
Nate Miller <nmiller2379@gmail.com>
2024-08-14 08:14:06 -0400

    initial commit

---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function TodoList() {
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://dummyjson.com/todos?limit=5")
+      .then((response) => {
+        setTodos(response.data.todos);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
